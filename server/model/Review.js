780f6fccd371221655ee4ba4b02ad0304494b7ae
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2023-03-02 09:55:38 +0700

    add ServiceProvider and Review models

---
+++ server/model/Review.js
@@ -0,0 +1,27 @@
+import { model, Schema } from "mongoose";
+
+const reviewSchema = new Schema(
+  {
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    serviceProvider: {
+      type: Schema.Types.ObjectId,
+      ref: "ServiceProvider",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default model("Review", reviewSchema);
