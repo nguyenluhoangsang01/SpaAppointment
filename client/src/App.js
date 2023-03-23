d46e9d772ae205d324c569b46aa5f25770c15c9b
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2023-03-23 12:15:42 +0700

    add new components, default layout and new views

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,35 @@
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { Toaster } from "react-hot-toast";
+import { Route, Routes } from "react-router-dom";
+import ErrorFallback from "./components/ErrorFallback";
+import ScrollToTop from "./components/ScrollToTop";
+import { routes } from "./constants";
+import Default from "./layouts";
+import Loading from "./views/Loading";
+
 function App() {
-	return <h1 className="text-3xl font-bold underline">Spa Appointment App</h1>;
+	return (
+		<ErrorBoundary FallbackComponent={ErrorFallback} onReset={() => {}}>
+			<Toaster toastOptions={{ duration: 5000 }} />
+
+			<ScrollToTop />
+
+			<Suspense fallback={<Loading />}>
+				<Routes>
+					<Route element={<Default />}>
+						{routes.map((route) => (
+							<Route
+								key={route.path}
+								path={route.path}
+								element={route.element}
+							/>
+						))}
+					</Route>
+				</Routes>
+			</Suspense>
+		</ErrorBoundary>
+	);
 }
 
 export default App;
