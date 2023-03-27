e955a72c2b5e32470fdb4007f3728e4156a8d22e
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2023-03-27 19:49:06 +0700

    This is a new commit for what I originally planned to be amended

---
+++ client/src/components/Heading/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Heading = ({ children }) => {
+	return <h1 className="font-bold uppercase mb-8 text-2xl">{children}</h1>;
+};
+
+export default Heading;
--- client/src/layouts/index.jsx
+++ client/src/layouts/index.jsx
@@ -1,15 +1,25 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/Footer";
+import Heading from "../components/Heading";
 import Navbar from "../components/Navbar";
+import { convertPathname } from "../utils/helpers";
 
 const Default = () => {
+	const { pathname } = useLocation();
+
 	return (
 		<>
 			<Navbar />
 
 			<main className="mx-auto px-6 md:px-10 max-w-full pt-[56px] min-h-[calc(100vh-56px)]">
 				<div className="py-6">
+					<Heading>
+						{convertPathname(pathname) !== ""
+							? convertPathname(pathname)
+							: "Home"}
+					</Heading>
+
 					<Outlet />
 				</div>
 			</main>
