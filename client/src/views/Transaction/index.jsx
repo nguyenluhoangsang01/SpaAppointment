f651b0500dbbaa8ccd2866272218fe4a6aa3efae
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2023-03-30 12:33:27 +0700

    update transaction

--- client/src/views/Transaction/index.jsx
+++ client/src/views/Transaction/index.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { selectAuth } from "../../redux/slice/auth";
 
 const Transaction = () => {
+	// Router
+	const navigate = useNavigate();
+	// Redux
+	const { user } = useSelector(selectAuth);
+
+	useEffect(() => {
+		if (!user) navigate("/sign-in");
+	}, [navigate, user]);
+
 	return <div>Transaction</div>;
 };
 
